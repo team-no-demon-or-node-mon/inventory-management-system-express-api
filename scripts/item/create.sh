92e2c57c092d1fce1c8f7b6695d4b3efb6805394
ceconley4 <ceconley4@gmail.com>
2018-12-17 19:33:40 -0500

    created curl scripts for item

---
+++ scripts/item/create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/items"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "item": {
+      "upc": "'"${UPC}"'",
+      "description": "'"${DESCRIPTION}"'",
+      "price": "'"${PRICE}"'",
+      "cost": "'"${COST}"'",
+      "quantity": "'"${QUANTITY}"'",
+      "ads": "'"${ADS}"'"
+    }
+  }'
+
+echo
